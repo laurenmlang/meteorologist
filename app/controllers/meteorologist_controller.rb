27be8ef63ca7df5b5b68e98258d899320fbe34f4
laurenmlang <laurenmlang@gmail.com>
2015-07-22 15:31:31 -0500

    Final

--- app/controllers/meteorologist_controller.rb
+++ app/controllers/meteorologist_controller.rb
@@ -16,33 +16,38 @@ class MeteorologistController < ApplicationController
     # A URL-safe version of the street address, with spaces and other illegal
     #   characters removed, is in the string url_safe_street_address.
     # ==========================================================================
+
+       # coord = GeocodingController.new
+       
+
+       #  @current_temperature = coord.street_to_coords
      
         @g_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+url_safe_street_address
         
           require 'json'
-          parsed_data = JSON.parse(open(@g_url).read)
+          p_data = JSON.parse(open(@g_url).read)
 
-          @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+          @lati = p_data["results"][0]["geometry"]["location"]["lat"]
 
-          @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+          @longi = p_data["results"][0]["geometry"]["location"]["lng"]
 
-          lat_s = @latitude.to_s
-          lng_s = @latitude.to_s
+          lat_s = @lati.to_s
+          lng_s = @lati.to_s
 
           @combine_url = "https://api.forecast.io/forecast/e6befb409a058d889af1b3bd5e71ed02/"+lat_s+","+lng_s
 
           require 'json'
-          parsed_combine = JSON.parse(open(@combine_url).read)
+          p_combine = JSON.parse(open(@combine_url).read)
 
-                @current_temperature = parsed_combine["currently"]["temperature"]
+            @current_temperature = p_combine["currently"]["temperature"]
 
-             @current_summary = parsed_combine["currently"]["summary"]
+            @current_summary = p_combine["currently"]["summary"]
 
-            #@summary_of_next_sixty_minutes = parsed_combine["minutely"]["summary"]
+           # @summary_of_next_sixty_minutes = p_combine["minutely"]["summary"]
 
-          @summary_of_next_several_hours = parsed_combine["hourly"]["summary"]
+            @summary_of_next_several_hours = p_combine["hourly"]["summary"]
 
-          @summary_of_next_several_days = parsed_combine["daily"]["summary"]
+            @summary_of_next_several_days = p_combine["daily"]["summary"]
 
     render("street_to_weather.html.erb")
   end
